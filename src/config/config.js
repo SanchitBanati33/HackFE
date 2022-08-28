f903f740dffb04472d01730c47e8ef44cf11725d
DishaAgarwalla <dishaagarwalla2001@gmail.com>
2022-08-28 11:41:28 +0530

    mainnet

--- src/config/config.js
+++ src/config/config.js
@@ -12,7 +12,7 @@ const configTestnet = {
   ethMainnetUrl: `https://eth-mainnet.g.alchemy.com/v2/QR5C5zzZSEnBkW8aQKWBkoblpPjENnwd`,
   contractAddress: "0x4137cF37598EE871d1F4A6DEE9188217Ed40c649",
   ethBcnMomentsCreatorAddress: "0x70c1EA05E2A54DfFE1088D4A54CB1a6C25c9077c",
-  ethCcMomentsCreatorAddress: "0x66Dc3BFCD29E24fDDeE7f405c705220E6142e4cD", // "0x99354ac00459C0D601760fd9c00329444D60F614",
+  ethCcMomentsCreatorAddress: "0x66Dc3BFCD29E24fDDeE7f405c705220E6142e4cD",
   sio: {
     id: "05dd4c3b-6635-4694-a4f4-11740b82df65",
     decentralizedId: 111,
@@ -21,7 +21,7 @@ const configTestnet = {
 
 const configMainnet = {
   chainId: 137,
-  apiBaseUrl: "https://prod.ethbarcelona.kraznikunderverse.com",
+  apiBaseUrl: "https://honetpot.kraznikunderverse.com",
   authOptions: {
     headers: {
       validate: process.env.REACT_APP_VALIDATE_TOKEN,
@@ -33,10 +33,12 @@ const configMainnet = {
   ethMainnetUrl: `https://eth-mainnet.g.alchemy.com/v2/QR5C5zzZSEnBkW8aQKWBkoblpPjENnwd`,
   contractAddress: "0xE3A161EdD679fC5ce2dB2316a4B6f7ab33a8eD6A",
   ethBcnMomentsCreatorAddress: "0xDb036eE95eA5B621559d44f6A720D8fDBCD9c43C",
-  ethCcMomentsCreatorAddress: "0x53aa9ACda320e8A88aB42dd30f446927e92f5D15",
+  ethCcMomentsCreatorAddress: "0x4Bf857db91e4ef45578c885a6D163B4a643d61b5",
   sio: {
-    id: "707eabb4-bd4f-4c71-a154-ecf614ae56ee", // ETH BCN Donations
-    decentralizedId: 43,
+    // id: "707eabb4-bd4f-4c71-a154-ecf614ae56ee", // ETH BCN Donations
+    // decentralizedId: 43,
+    id: "0246c974-024c-416c-b4c2-23c7207439b4", // Gitcoin
+    decentralizedId: 58,
   },
 };
 
--- src/ethereum/web3.js
+++ src/ethereum/web3.js
@@ -1,5 +1,6 @@
 // import Web3 from "web3";
 const Web3 = require("web3");
+const { config } = require("../config/config");
 
 let web3;
 
@@ -8,10 +9,7 @@ if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
   // web3 = new Web3(window.web3.currentProvider);
   web3 = new Web3(window.ethereum);
 } else {
-  const provider = new Web3.providers.HttpProvider(
-    // "https://polygon-mumbai.g.alchemy.com/v2/T95ylN-bR7zmaXiaZkP0R1sOObutgv-M"
-    "https://polygon-mainnet.g.alchemy.com/v2/VX9hsINm25dNmmBqKi9ydn_iBW4sio4i"
-  );
+  const provider = new Web3.providers.HttpProvider(config.alchemyUrl);
 
   // "https://rinkeby.infura.io/v3/97c2d52095a84da7a0b710a8daa16acf"
   web3 = new Web3(provider);
